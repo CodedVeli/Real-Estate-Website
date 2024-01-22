5f33aa900231b1b6e79c0efc1475188de4be58a2
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2024-01-22 19:24:13 +0300

    Navbar

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import photo1 from "../assets/pic1.jpg";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="header">
+      <img src={photo1} alt="house" />
+    </div>
+  );
+}
+
+export default Home;
