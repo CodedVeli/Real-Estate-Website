e6bd6b218968e0f7f6da6ebfcddb6c8f2307a30d
Navashub <navasmuller01@gmail.com>
2024-01-23 17:09:35 +0300

    Backend models done , and the migration

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from sqlalchemy import Integer, ForeignKey, Boolean, Float, String
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -10,8 +11,28 @@ class User(db.Model):
     email = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
 
-class House(db.Model):
-    pass
+class Property(db.Model):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    bedrooms = db.Column(db.Integer, nullable=False)
+    bathrooms = db.Column(db.Integer, nullable=False)
+    parking = db.Column(db.Boolean, nullable=False)
+    furnished = db.Column(db.Boolean, nullable=False)
+    offer = db.Column(db.Boolean, nullable=False)
+    regular_price = db.Column(db.Float, nullable=False)
+    discounted_price = db.Column(db.Float, nullable=False)
+    location = db.Column(db.String(255), nullable=False)
 
 class Review(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    property_id = db.Column(db.Integer, db.ForeignKey('property.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text, nullable=True)
+
+
+    user = db.relationship('User', backref='reviews')
+    property = db.relationship('Property', backref='reviews')
