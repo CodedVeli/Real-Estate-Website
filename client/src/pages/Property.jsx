4867d0c2b7e25e6b731a28de20dfea9944f982ee
Hingaz <hhinga04@gmail.com>
2024-01-23 10:27:25 +0300

    Updated property page

--- client/src/pages/Property.jsx
+++ client/src/pages/Property.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React from 'react'
+import { useEffect, useState } from "react";
+
 
 function Property() {
-  return <div>Property</div>;
+  const [properties, setProperties] = useState([]);
+  const [error, setError] = useState(null);
+ 
+  
+  useEffect(() => {
+    fetch("https:/properties")
+      .then((r) => {
+        if (!r.ok) {
+          throw new Error(`HTTP error! Status: ${r.status}`);
+        }
+        return r.json();
+      })
+      .then((data) => {
+        console.log("Data from /properties:", data);
+        setProperties(data);
+      })
+      .catch((err) => {
+        console.error("Error fetching properties:", err.message);
+        setError(err.message);
+      });
+  }, []);
+
+  console.log(":", properties);
+  console.log("Error:", error);
+
+  
+  return (
+    <div>
+      <h2>All Properties</h2>
+      <ul>
+        {properties.map((property) => (
+          <li key={property.id}>
+            Property Name: {property.title} 
+            Desciption: {property.description}
+            Price: {property.price}
+            Location: {property.location}
+            {property.image_url}
+            {property.created_at}</li>
+
+        ))}
+      </ul>
+
+      
+    </div>
+  );
 }
 
-export default Property;
+
+
+export default Property;           
